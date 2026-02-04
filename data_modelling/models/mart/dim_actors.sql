cca2ec21cbb73c1538afe89e8fae2f856e4da0d2
Michael <59081538+Delta483711@users.noreply.github.com>
2026-02-04 20:27:34 +0000

    update column name

--- data_modelling/models/mart/dim_actors.sql
+++ data_modelling/models/mart/dim_actors.sql
@@ -1,7 +1,7 @@
 
 SELECT 
     row_number() OVER () AS actor_id,  -- generate a unique ID
-    SPLIT(actor, ',')  AS actor_name
+    SPLIT(actors, ',')  AS actor_name
 FROM (
     SELECT DISTINCT 
         actor_name   
