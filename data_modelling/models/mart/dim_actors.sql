5c858d022ee1894123e9b06c282094f0bcfffdda
Michael <59081538+Delta483711@users.noreply.github.com>
2026-02-04 20:23:10 +0000

    Update
    - Added new dim for actors

---
+++ data_modelling/models/mart/dim_actors.sql
@@ -0,0 +1,8 @@
+
+SELECT 
+    row_number() OVER () AS actor_id,  -- generate a unique ID
+    SPLIT(actor, ',')  AS actor_name
+FROM (
+    SELECT DISTINCT actor   
+    FROM {{ ref('int_netflix') }}
+) t 
