1a85d9277376c43fc44b034bcf1a51632e5ae1d8
Michael <59081538+Delta483711@users.noreply.github.com>
2026-02-07 13:45:17 +0000

    update dim actor to fix duplicates

--- data_modelling/models/mart/dim/dim_actors.sql
+++ data_modelling/models/mart/dim/dim_actors.sql
@@ -2,26 +2,17 @@
 -- It will be used to join with the bridge table to get the actor's name for each netflix title.
 
 WITH source AS (
-SELECT
-    *
-FROM
-     {{ ref('int_netflix') }}
+    SELECT TRIM(actor_name) AS actor_name
+    FROM {{ ref('int_netflix') }}
 ),
 
-actors AS (
-SELECT
-    row_number() OVER () AS actor_id,  -- generate a unique ID
-    actor_name 
-FROM    
-    source 
-
+unique_actors AS (
+    SELECT DISTINCT actor_name
+    FROM source
 )
 
 SELECT
-    actor_id,
-    TRIM(actor_name) AS actor_name
-FROM
-     actors
-
-
+    ROW_NUMBER() OVER (ORDER BY actor_name) AS actor_id,
+    actor_name
+FROM unique_actors
 
