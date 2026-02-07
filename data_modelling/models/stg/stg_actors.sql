4f8f74cbb9a190a5fd43f80f2d4a702bd11115da
Michael <59081538+Delta483711@users.noreply.github.com>
2026-02-07 09:02:51 +0000

    Dbt/07.02.2026 (#25)
    * update logic
    
    - Relocate int logic into stage model
    - Create stg_actor model to separate actors
    
    * update
    
    - Update schema
    - Update stg model
    
    * update indentation
    
    * update tests in schema
    
    * update extract for season
    
    * Remove not null test
    
    * update stg_actors
    
    * update profiles and update stg_actors
    
    * update project

---
+++ data_modelling/models/stg/stg_actors.sql
@@ -0,0 +1,10 @@
+WITH source AS (
+    SELECT *
+    FROM {{ ref('stg_netflix') }}
+)
+
+SELECT
+    s.show_id,
+    TRIM(a.actor_name) AS actor_name
+FROM source s
+CROSS JOIN LATERAL unnest(string_to_array(s.actors, ',')) AS a(actor_name)
