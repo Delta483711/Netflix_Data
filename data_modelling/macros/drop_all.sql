5700b4b832e1cc9b0e512d1decb16328e16def38
Michael <59081538+Delta483711@users.noreply.github.com>
2026-02-08 08:44:23 +0000

    Update drop all

--- data_modelling/macros/drop_all.sql
+++ data_modelling/macros/drop_all.sql
@@ -1,16 +1,20 @@
 {% macro drop_all() %}
 
-{# Loop over all models in the project and drop them #}
+{% if target.name == 'prod' %}
+    {{ exceptions.raise_compiler_error("drop_all is disabled in prod") }}
+{% endif %}
+
 {% for model in graph.nodes.values() if model.resource_type == 'model' %}
     {% if execute %}
         {% do log("Dropping model: " ~ model.name, info=True) %}
         {% set sql %}
-            {% if model.materialized == 'view' %}
-                drop view if exists {{ model.schema }}.{{ model.identifier }} cascade;
+            {% if model.config.materialized == 'view' %}
+                drop view if exists {{ model.schema }}.{{ model.identifier }};
             {% else %}
-                drop table if exists {{ model.schema }}.{{ model.identifier }} cascade;
+                drop table if exists {{ model.schema }}.{{ model.identifier }};
             {% endif %}
         {% endset %}
+        {% do log(sql, info=True) %}
         {{ run_query(sql) }}
     {% endif %}
 {% endfor %}
