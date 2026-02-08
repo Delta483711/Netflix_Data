2416b0bb9425a764ab789db98cd2182339c9db3e
Michael <59081538+Delta483711@users.noreply.github.com>
2026-02-08 08:49:40 +0000

    update drop_all macro

--- data_modelling/macros/drop_all.sql
+++ data_modelling/macros/drop_all.sql
@@ -4,19 +4,26 @@
     {{ exceptions.raise_compiler_error("drop_all is disabled in prod") }}
 {% endif %}
 
-{% for model in graph.nodes.values() if model.resource_type == 'model' %}
-    {% if execute %}
-        {% do log("Dropping model: " ~ model.name, info=True) %}
-        {% set sql %}
-            {% if model.config.materialized == 'view' %}
-                drop view if exists {{ model.schema }}.{{ model.identifier }};
-            {% else %}
-                drop table if exists {{ model.schema }}.{{ model.identifier }};
-            {% endif %}
-        {% endset %}
-        {% do log(sql, info=True) %}
-        {{ run_query(sql) }}
+{% for node in graph.nodes.values() if node.resource_type == 'model' %}
+
+    {# Skip ephemeral models #}
+    {% if node.config.materialized == 'ephemeral' %}
+        {% continue %}
     {% endif %}
+
+    {% set relation = adapter.get_relation(
+        database=node.database,
+        schema=node.schema,
+        identifier=node.identifier
+    ) %}
+
+    {% if relation %}
+        {% do log("Dropping relation: " ~ relation, info=True) %}
+        {{ adapter.drop_relation(relation) }}
+    {% else %}
+        {% do log("Skipping (no relation): " ~ node.name, info=True) %}
+    {% endif %}
+
 {% endfor %}
 
 {% endmacro %}
